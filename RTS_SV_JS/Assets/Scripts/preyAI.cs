b3ff2bfcb2be2401aac478315be7cd627121f902
quentinou34 <quentin.andre-97@laposte.net>
2019-11-19 15:38:09 +0100

    Moar kiwis
    Reproduction

--- RTS_SV_JS/Assets/Scripts/preyAI.cs
+++ RTS_SV_JS/Assets/Scripts/preyAI.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class preyAI : MonoBehaviour
 {
-
-
-
+   
     private int health;
-    int time ;
+    public int lookingformate = 0;
+    public int timeLeft; //grow and mate time
+    public int timeLeftMax=60;
+    private int mated = 0;
+    private float range = 1.5f;
+    private GameObject mate;
+    private bool hasMate = false;
+    private int time = 0; // movement time
+
+    
+    public GameObject pref;
+
     [SerializeField]
     private GameObject infoPanel;
     private bool infoPanelActive = false;
@@ -22,13 +31,15 @@ public class preyAI : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        rb2d.drag = 20;
-        
+        rb2d.drag = 5;
+        timeLeft = timeLeftMax;
         rb2d.gravityScale = 0;
         this.health = 2;
         this.time = 0;
-
-    }
+        StartCoroutine("LoseTime");
+        
+    
+    }   
 
     // Update is called once per frame
     void Update()
@@ -38,34 +49,91 @@ public class preyAI : MonoBehaviour
     }
     void UpdateGFX()
     {
-
+        if (lookingformate <= 0) { gfx.transform.localScale = new Vector3(0.5f, 0.5f, 1f); }
+        else { gfx.transform.localScale = new Vector3(1f, 1f, 1f); }
         //Flip updating
         if (rb2d.velocity.x >= 0.01f)
         {
-            gfx.localScale = new Vector3(1f, 1f, 1f);
+            Vector3 n = gfx.localScale;
+            n.x = Mathf.Abs(n.x);
+            gfx.localScale = n;
         }
         else if (rb2d.velocity.x <= -0.01f)
         {
-            gfx.localScale = new Vector3(-1f, 1f, 1f);
+            Vector3 n = gfx.localScale;
+            n.x = Mathf.Abs(n.x)*-1;
+            gfx.localScale = n;
         }
     }
     void FixedUpdate()
     {
         if (alive)
         {
-            if (time == 30)
-            {
-                Vector2 mv = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-                rb2d.velocity = mv;
+            if (timeLeft <= 0) { lookingformate += 1; timeLeft = timeLeftMax; mate = null; hasMate = false; }
 
-                time = 0;
+
+            if (lookingformate > 1 && lookingformate >= mated)
+            {
+                if (!hasMate || !mate.GetComponent<preyAI>().alive)
+                {
+                    GameObject[] possibleMates = GameObject.FindGameObjectsWithTag("Prey");
+                    float minDIst = Mathf.Infinity;
+                    foreach (GameObject m in possibleMates)
+                    {
+                        float dist = Vector3.Distance(m.GetComponent<Transform>().localPosition, this.transform.localPosition);
+                        if (m.GetComponent<preyAI>().GetInstanceID() != this.GetInstanceID() && dist < minDIst && m.GetComponent<preyAI>().lookingformate > 1 && m.GetComponent<preyAI>().lookingformate > m.GetComponent<preyAI>().mated && m.GetComponent<preyAI>().alive)
+                        {
+                            mate = m;
+                            hasMate = true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (IsTargetInRange())
+                    {
+                        mated++;
+                        hasMate = false;
+                        mate = null;
+
+                        Instantiate(pref);
+                        
+                    }
+                    else
+                    {
+                        if (time >= 30)
+                        {
+                            Vector2 direction = mate.GetComponent<Transform>().localPosition - this.transform.position  ;
+                            rb2d.velocity = direction.normalized;
+                            time = 0;
+                        }
+                        else
+                        {
+                            time++;
+                        }
+
+                    }
+
+                }
             }
             else
             {
-                time++;
+
+                if (time >= 30)
+                {
+                    Vector2 mv = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                    rb2d.velocity = mv.normalized;
+                    time = 0;
+                }
+                else
+                {
+                    time++;
+                }
             }
         }
     }
+   
     public int GetHealth()
     {
         return this.health;
@@ -82,5 +150,18 @@ public class preyAI : MonoBehaviour
     public void Die()
     {   
         if(!alive)  Destroy(this.gameObject);
+    }
+    private bool IsTargetInRange()
+    {
+        return (Vector2.Distance(this.transform.position, mate.GetComponent<Transform>().localPosition) <= range);
+    }
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+
+        }
     }
 }
